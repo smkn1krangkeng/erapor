713a77444c2ac57711bb0bbe52ea52a45203fa24
ozonerik <ozonerik@gmail.com>
2021-08-16 15:35:28 +0700

    update layout

--- dev/resources/views/layout/frontend/main.blade.php
+++ dev/resources/views/layout/frontend/main.blade.php
@@ -4,12 +4,15 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="eRapor SMKN 1 Krangkeng">
+    <meta name="author" content="M. Ade Erik, S.Pd.">
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <!-- Bootstrap Icon -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
     @stack('css')
-
+    <!-- Custom styles for this template -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <title>@yield('judul_hal')</title>
   </head>
   <body>
--- dev/resources/views/pages/app.blade.php
+++ dev/resources/views/pages/app.blade.php
@@ -1,12 +1,12 @@
 @extends('layout.frontend.main')
 @section('judul_hal','Halaman Coba')
 @push('css')
-<h4>ini css</h4>
+<h4>ini push css</h4>
 @endpush
 @push('scripts')
-<h4>ini script</h4>
+<h4>ini push script</h4>
 @endpush
 @section('konten')
 <h1>Hello World</h1>
-<i class="bi-alarm"></i>Bhe
+<i class="bi-alarm"></i> ini contoh icon bootstrap 5
 @endsection
